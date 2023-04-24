e859bcc200c05e19d024504a0fb724fb5cac69f1
biscuit-lee <sophearothprum168@gmail.com>
2023-04-24 19:33:59 +0200

    added basic style to index page

---
+++ job-hunter/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return ( 
+    <div class="container">
+      <div class="rank"> 
+          <h2> Framework ranks by popularity in job listings</h2>
+          <li> First </li>
+          <li> Second </li>
+      </div>  
+
+      <div class="rank"> 
+          <h2> Skills ranks by popularity in job listings</h2>
+          <li> First </li>
+          <li> Second </li>
+      </div>  
+
+      <div class="rank"> 
+          <h2> Programming Language ranks by popularity in job listings</h2>
+          <li> First </li>
+          <li> Second </li>
+      </div>  
+
+      <div class="rank"> 
+          <h2> Soft skills ranks by popularity in job  listings</h2>
+          <li> First </li>
+          <li> Second </li>
+      </div>  
+    
+    </div>
+  );
+}
+
+export default App;
