5f09681d75436d29c0018a06b7b6087741fc6825
biscuit-lee <sophearothprum168@gmail.com>
2023-05-04 19:19:46 +0200

    oops

--- job-hunter/src/HomePage.js
+++ job-hunter/src/HomePage.js
@@ -2,38 +2,7 @@ import axios from 'axios'
 import { useEffect } from 'react';
 
 function HomePage(){
-    // accessing api key from env (jooble)
-    const jooble_url = 'https://jooble.p.rapidapi.com/?keywords=python';
-    const api_key = '6ecc17f6ffmsh74a4769f07de774p11ab8ejsncb8e4c0fe5c4';
 
-
-    const data = null;
-
-    const xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-    
-    xhr.addEventListener('readystatechange', function () {
-        if (this.readyState === this.DONE) {
-            console.log(this.responseText);
-        }
-    });
-    console.log(api_key);
-    xhr.open('GET', jooble_url);
-    xhr.setRequestHeader('content-type', 'application/octet-stream');
-    xhr.setRequestHeader('X-RapidAPI-Key', api_key);
-    xhr.setRequestHeader('X-RapidAPI-Host', 'jooble.p.rapidapi.com');
-    
-    xhr.send(data);
-/*
-    useEffect( () => {
-        const response = axios.get(url,{headers : {
-            'Authorization': `Bearer ${client_id}:${secret}`
-            }})
-        .then(response => console.log(response)
-        .catch(error => (console.log(error))));
-    },[]);
-
-*/
     
 
     return(
